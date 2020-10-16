d14ccf4342134135594bf5cb7593e8176f20368b
Diego Arthur <digo.arthur@hotmail.com>
2020-10-16 20:08:14 -0300

    add

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-
-var imgGitHub = document.getElementById("iconGithub")
-
-
-
-function setNewImage(valorIcon){
+//****************************************** 
+// EFEITO ÍCONE SOCIAIS HEADER
+//******************************************
 
 
+//****************************************** 
+// EFEITO DIGITAR HEADER
+//******************************************
 
-   switch(valorIcon){
-    case 1:
-        document.getElementById("iconGithub").src = "img/icon_Github_Color.png";
 
-        break;
-    case 2:
-        document.getElementById("iconLinkedin").src = "img/icon_Linkedin_Color.png";
-        break;
-    case 3:
-        document.getElementById("iconFacebook").src = "img/icon_Facebook_Color.png";
-        break;
-    case 4:
-        document.getElementById("iconInstagram").src = "img/icon_Instagram_Color.png";
-        break;
-    case 5:
-        document.getElementById("iconTwitter").src = "img/icon_Twitter_Color.png";
-        break;
-};//End Switch
-    
+
+
+
+function setNewImage(valorIcon) {
+
+
+
+    switch (valorIcon) {
+        case 1:
+            document.getElementById("iconGithub").src = "img/icon_Github_Color.png";
+
+            break;
+        case 2:
+            document.getElementById("iconLinkedin").src = "img/icon_Linkedin_Color.png";
+            break;
+        case 3:
+            document.getElementById("iconFacebook").src = "img/icon_Facebook_Color.png";
+            break;
+        case 4:
+            document.getElementById("iconInstagram").src = "img/icon_Instagram_Color.png";
+            break;
+        case 5:
+            document.getElementById("iconTwitter").src = "img/icon_Twitter_Color.png";
+            break;
+    };//End Switch
+
 }
 
-function setOldImage(valorIcon){
-    
-    switch(valorIcon){
+function setOldImage(valorIcon) {
+
+    switch (valorIcon) {
         case 1:
             document.getElementById("iconGithub").src = "img/icon_Github.png";
             break;
@@ -49,3 +57,20 @@ function setOldImage(valorIcon){
     };
 
 }
+
+
+
+
+function digitaTexto() {
+    const element = document.querySelector('#descricaoQuemSoueu');
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = '';
+    console.log(textArray);
+    textArray.forEach((letra,i) => {
+        setTimeout(()=> element.innerHTML+=letra,75*i  );
+
+    });
+}
+
+
+
